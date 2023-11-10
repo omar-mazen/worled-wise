262f73892c1f7b4e2562133531f8cb236d26dbc0
omar mazen <86468135+omar-mazen@users.noreply.github.com>
2023-11-10 11:44:12 +0200

    Update server.js

--- api/server.js
+++ api/server.js
@@ -1,22 +1,38 @@
-const jsonServer = require("json-server");
+const jsonServer = require('json-server');
+const fs = require('fs');
+const path = require('path');
 
 const server = jsonServer.create();
 
-// Uncomment to allow write operations
-const fs = require("fs");
-const path = require("../db.json");
-const filePath = path.join("db.json");
-const data = fs.readFileSync(filePath, "utf-8");
+const filePath = path.join(__dirname, 'db.json'); // Adjust the path to your db.json file
+const data = fs.readFileSync(filePath, 'utf-8');
 const db = JSON.parse(data);
+
 const router = jsonServer.router(db);
-
-// Comment out to allow write operations
-// const router = jsonServer.router("db.json");
-
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
+
 server.use(router);
+
+// Enable CORS for all routes
+server.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
+// Support options preflight request
+server.options('*', (req, res) => {
+    res.sendStatus(200);
+});
+
+// Vercel automatically assigns a port
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`JSON Server is running on port ${PORT}`);
+});
 
 // Export the Server API
 module.exports = server;
